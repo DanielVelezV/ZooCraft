7a5900dba098d6911ab2fcdee8290ec47774a30e
BuildTools <unconfigured@null.spigotmc.org>
2021-01-23 19:36:18 -0500

    Plugin Template

---
+++ src/main/java/com/elwarriorcito/plugins/zoocraft/ZooCraft.java
@@ -0,0 +1,17 @@
+package com.elwarriorcito.plugins.zoocraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZooCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
