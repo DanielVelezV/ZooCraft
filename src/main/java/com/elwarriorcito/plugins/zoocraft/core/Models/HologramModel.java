200d8c69f09e5bea86b9dc7ad9deb62c669457b7
BuildTools <unconfigured@null.spigotmc.org>
2021-01-24 12:04:27 -0500

    Documented the HologramModel.

--- src/main/java/com/elwarriorcito/plugins/zoocraft/core/Models/HologramModel.java
+++ src/main/java/com/elwarriorcito/plugins/zoocraft/core/Models/HologramModel.java
@@ -4,14 +4,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class HologramModel {
 
-    public HashMap<Integer, ArmorStand> lines;
-    private int Linescount;
+    public HashMap<Integer, ArmorStand> lines; //Line : Text/Entity
+    private int Linescount; //Count all the lines that an Hologram have
 
     public HologramModel(){
         this.lines = new HashMap<Integer, ArmorStand>();
@@ -19,15 +17,15 @@ public class HologramModel {
 
     }
 
-    public void createLine(Location loc, String Line){
-       ArmorStand firstLine = (ArmorStand) loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
-       firstLine.setCustomName(c(Line));
-       firstLine.setCustomNameVisible(true);
-       firstLine.setGravity(false);
-       firstLine.setInvisible(true);
-       firstLine.setInvulnerable(true);
-       Linescount++;
-       lines.put(Linescount, firstLine);
+    public void createLine(Location loc, String Line){ //Always call this for the firstLine
+       ArmorStand firstLine = (ArmorStand) loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND); //First Line
+       firstLine.setCustomName(c(Line)); //The line text (Can use color codes from Minecraft)
+       firstLine.setCustomNameVisible(true); //Custom name visibility
+       firstLine.setGravity(false); //No gravity
+       firstLine.setInvisible(true); //Invisible
+       firstLine.setInvulnerable(true); //Invulnerability (Not working somehow)
+       Linescount++; //Add 1 to the lines count
+       lines.put(Linescount, firstLine); //add this line to the line list
     }
 
     public void addLine(String Line){
@@ -43,16 +41,16 @@ public class HologramModel {
         lines.put(Linescount, line);
     }
 
-    public void removeLine(int lineId){
+    public void removeLine(int lineId){ //Remove line by Line ID
         if (lines.get(lineId) != null){
             lines.get(lineId).remove();
         }
     }
 
-    public int getLinescount(){return Linescount;}
+    public int getLinescount(){return Linescount;} //Get how many lines this Hologram have
 
 
-    private String c(final String c) {
+    private String c(final String c) { //Color codes from Minecraft
         return ChatColor.translateAlternateColorCodes('&', c);
     }
 
